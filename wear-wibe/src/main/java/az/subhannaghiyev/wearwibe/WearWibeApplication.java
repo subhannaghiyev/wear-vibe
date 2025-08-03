613037de3e17d6709146c0ee7537cbfa4f98ad66
subhan.naghiyev <subhan.naghiyev@guavapay.com>
2025-08-04 03:30:09 +0400

    Initial commit

---
+++ wear-wibe/src/main/java/az/subhannaghiyev/wearwibe/WearWibeApplication.java
@@ -0,0 +1,13 @@
+package az.subhannaghiyev.wearwibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearWibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearWibeApplication.class, args);
+	}
+
+}
---
+++ wear-wibe/src/test/java/az/subhannaghiyev/wearwibe/WearWibeApplicationTests.java
@@ -0,0 +1,13 @@
+package az.subhannaghiyev.wearwibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearWibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
